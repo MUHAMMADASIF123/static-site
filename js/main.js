c5667ffca2ed24dda6da128e9588c1831c158f7e
MUHAMMADASIF123 <asifaliashrafali@gmail.com>
2023-03-10 12:47:09 +0500

    navbar collapse

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log("hello");
+// nav hide
+// let navBar = document.querySelectorAll(".nav-link");
+// let navCollapse = document.querySelector(".navbar-collapse.collapse");
+// navBar.forEach(function (a) {
+//   a.addEventListener("click", function () {
+//     navCollapse.classList.remove("show");
+//   });
+// });
+
+(function ($) {
+  $(document).ready(function () {
+    $("ul.dropdown-menu [data-toggle=dropdown]").on("click", function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      $(this).parent().siblings().removeClass("open");
+      $(this).parent().toggleClass("open");
+    });
+  });
+})(jQuery);
